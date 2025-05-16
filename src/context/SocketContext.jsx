7ab3891acd0733038a5bfa5a7de22fd9c620f4d4
Aryan <aryan20072020@gmail.com>
2025-05-16 11:09:29 +0530

    v2

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:5000"));
+    setSocket(io("https://socketweb56.onrender.com"));
   }, []);
 
   useEffect(() => {
