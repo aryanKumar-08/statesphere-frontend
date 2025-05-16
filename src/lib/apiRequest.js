7917427d115a74d41c31e1d58e04ba84c06f43ea
Aryan <aryan20072020@gmail.com>
2025-05-16 12:21:03 +0530

    p

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiRequest = axios.create({
-    baseURL : "https://statesphere-backend-1.onrender.com/api",
-    withCredentials: true
+    baseURL : "https://statesphere-backend-1.onrender.com",
+    withCredentials: true,
 })
 
 export default apiRequest;
