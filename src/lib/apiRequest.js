6ff33f7ee1bdaef86506696fb2d474457db0d9f8
Aryan <aryan20072020@gmail.com>
2025-05-16 14:16:58 +0530

    ttt

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,29 +1,9 @@
-// import axios from "axios";
-// const apiRequest = axios.create({
-//     baseURL : "https://statesphere-backend-1.onrender.com/api",
-//     withCredentials: true,
-// })
-
-// export default apiRequest;
-
-
-
 import axios from "axios";
-
 const apiRequest = axios.create({
-  baseURL: "https://statesphere-backend-1.onrender.com/api",
-  withCredentials: true, // Send cookies with every request
-});
-
-// Intercept 401 errors to handle logout
-apiRequest.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = "/login"; // Redirect to login
-    }
-    return Promise.reject(error);
-  }
-);
+    baseURL : "https://statesphere-backend-1.onrender.com/api",
+    withCredentials: true,
+})
 
 export default apiRequest;
+
+
